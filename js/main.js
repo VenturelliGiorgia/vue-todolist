72fd58d6a1b803491fcf303cbdedfedc2eba1b97
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2022-11-23 17:37:43 +0100

    aggiunta e eliminazione lista

--- js/main.js
+++ js/main.js
@@ -8,17 +8,29 @@ createApp({
                     text: "fare la spesa",
                     done: true
                 },
-                {
-                    text: "ritirare i panni",
-                    done: false
-                },
             ],
-            newTaskData: {
-                text: ''
-            }
+
+            newTaskText: ""
         }
     },
 
-    methods: {}
+    methods: {
+        addList() {
+            this.taskList.push({ 
+                text: this.newTaskText, //vado a inserire la stringa nell'array di oggetti
+                done: false
+            })
+
+        },
+
+        delateList(index) { //passo l'indice della lista che bisogna eliminare
+            const conferma = confirm('Are you sure you want to delete')
+
+            if (conferma) {
+                this.taskList.splice(index, 1);
+            }
+
+        }
+    }
 
 }).mount("#app")
